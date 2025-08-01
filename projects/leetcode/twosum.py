8f4db3cddc2bf498f9a20aea7a5a4f5a39fa83c3
LinoxYoung <linoxzinhocontado@gmail.com>
2025-08-01 20:51:11 -0300

    new two sum

--- projects/leetcode/twosum.py
+++ projects/leetcode/twosum.py
@@ -1,12 +1,11 @@
-target = 5
-nums = [1,4,71,614,6,12,8]
-count_2 =  0
-count = 0
-while count_2 <= len(nums):
-    while count <= len(nums)-1:
-        count += 1
-        if count <= len(nums)-1 and count_2 <= len(nums)-1 and ((nums[count] + nums[count_2]) == target) and count_2 != count:
-            print([count_2, count])
-            break
-    count_2 += 1
-    count = 0
+nums = [2,7,11,15]
+target = 9
+c = 0
+c2 = len(nums)-1
+while c < len(nums)-1:
+    while c2 > c:
+        if nums[c]+nums[c2] == target:
+            print([c,c2])
+        c2 -= 1
+    c += 1
+    c2 = len(nums)-1
